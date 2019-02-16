b052d013efa1f4adf8bcbd62a8aa70f355f600a4
cmmani <45930745+cmmani@users.noreply.github.com>
2019-02-16 11:06:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1><?php echo "HELLO MANI"; ?></h1>https://aws.amazon.com/console/
-<h1><?php echo "HELLO"; ?>
-  <h1><?php echo "HELphpapp.com"; ?>
+<h1><?php echo "HELLO"; ?></h1>
+  <h1><?php echo "HELphpapp.com"; ?></h1>
